e8341de0481054309d97fc6d04f38334605ebf89
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2025-09-12 15:03:15 +0530

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.java
@@ -0,0 +1,19 @@
+// Last updated: 12/09/2025, 15:03:13
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int ans[]= new int [nums.length];
+        int pos=0;
+        int neg =1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos+=2;
+            }
+            else{
+                ans[neg]=nums[i];
+                neg+=2;
+            }
+        }
+        return ans;
+    }
+}

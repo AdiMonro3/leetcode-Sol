9199ccc3c114170de7746e4e10debeaaa7180ed2
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2025-09-13 01:51:53 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,16 @@
+// Last updated: 13/09/2025, 01:51:53
+class Solution {
+    
+    public int climbStairs(int n) {
+       int prev2 = 1;
+       int prev = 1;
+  
+         for(int i=2; i<=n; i++){
+            int cur_i = (prev2+ prev);
+            prev2 = prev;
+            prev= cur_i;
+        }
+        return prev;
+   
+    }
+}

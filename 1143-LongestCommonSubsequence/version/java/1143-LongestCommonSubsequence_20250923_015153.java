a631169c3d058da1fe037a22841cd29fba890e8f
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2025-09-23 01:51:53 +0530

    Create 1143-LongestCommonSubsequence

---
+++ 1143-LongestCommonSubsequence/version/java/1143-LongestCommonSubsequence_20250923_015153.java
@@ -0,0 +1,38 @@
+// Last updated: 23/09/2025, 01:51:53
+class Solution {
+    // rdcursion and memoization
+    public static int lcs(String text1,String text2,int i,int j,int dp[][]){
+        if(i<0 || j<0) return 0;
+        int m=0;
+        int n=0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(text1.charAt(i)==text2.charAt(j)){
+             m=1+lcs(text1,text2,i-1,j-1,dp);
+        }
+        else{
+         n=Math.max(lcs(text1,text2,i-1,j,dp),lcs(text1,text2,i,j-1,dp));
+        }
+        return dp[i][j]=m+n;
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        int [][]dp= new int[text1.length()][text2.length()];
+       for (int[] row : dp) {
+         Arrays.fill(row, -1);
+        }
+        return lcs(text1,text2,text1.length()-1,text2.length()-1, dp);
+
+    // // tabulation approach
+    // int i=text1.length();
+    // int j=text2.length();
+    // int dp[][]= new int [i+1][j+1];
+    // for(int row=1;row<i+1;row++){
+    //     for(int col=1;col<j+1;col++){
+    //          if(text1.charAt(row-1)==text2.charAt(col-1)){
+    //             dp[row][col]=1+dp[row-1][col-1];
+    //          }
+    //          else dp[row][col]=Math.max(dp[row-1][col],dp[row][col-1]);
+    //     }
+    // }
+    // return dp[i][j];
+    }
+}

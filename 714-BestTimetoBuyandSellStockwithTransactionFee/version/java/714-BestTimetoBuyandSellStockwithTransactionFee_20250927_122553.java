80f1dbcaf79c7040fb5eec55121bfa97e7347545
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2025-09-27 12:25:54 +0530

    Create 714-BestTimetoBuyandSellStockwithTransactionFee

---
+++ 714-BestTimetoBuyandSellStockwithTransactionFee/version/java/714-BestTimetoBuyandSellStockwithTransactionFee_20250927_122553.java
@@ -0,0 +1,24 @@
+// Last updated: 27/09/2025, 12:25:53
+class Solution {
+     public int helper(int [] arr,int n,int idx,int buy,int dp[][],int fees){
+        if(idx>=n) return 0;
+        if(dp[idx][buy]!=-1) return dp[idx][buy];
+        int profit=0;
+        if(buy==1){
+            profit=Math.max(-arr[idx]+helper(arr,n,idx+1,0,dp,fees),helper(arr,n,idx+1,1,dp,fees));
+        }else{
+            profit=Math.max(arr[idx]-fees+helper(arr,n,idx+1,1,dp,fees),helper(arr,n,idx+1,0,dp,fees));
+        }
+        return dp[idx][buy]=profit;
+    }
+    public int maxProfit(int[] prices, int fee) {
+         int n=prices.length;
+        int [][]dp=new int [n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return helper(prices,n,0,1,dp,fee);
+    }
+}

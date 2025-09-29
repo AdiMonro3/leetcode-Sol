d0bd9fb777d97bc529d385d5996eb833a38484e0
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2025-09-30 01:04:31 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/version/java/55-JumpGame_20250930_010430.java
@@ -0,0 +1,14 @@
+// Last updated: 30/09/2025, 01:04:30
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int maxIndex=0;
+        for(int i=0;i<n;i++){
+              if (i > maxIndex) {
+                return false;
+            }
+              maxIndex = Math.max(maxIndex, i + nums[i]);
+        }
+        return true;
+    }
+}

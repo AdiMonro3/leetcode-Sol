3d58a5c06ac8e0f1a64712d7b16cd1499e0addfa
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2025-11-20 17:05:24 +0530

    Create 1930-UniqueLength-3PalindromicSubsequences

---
+++ 1930-UniqueLength-3PalindromicSubsequences/version/java/1930-UniqueLength-3PalindromicSubsequences_20251120_170523.java
@@ -0,0 +1,34 @@
+// Last updated: 20/11/2025, 17:05:23
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int[] first=new int[26];
+        int[] last=new int[26];
+
+        Arrays.fill(first,-1);
+        Arrays.fill(last,-1);
+
+        for(int i=0;i<s.length();i++){
+            int x=s.charAt(i)-'a';
+
+            if(first[x]==-1){
+                first[x]=i;
+            }
+            last[x]=i;
+        }
+        int ans=0;
+        for(int i=0;i<26;i++){
+
+            int u=first[i];
+            int v=last[i];
+            if(u==v || u==-1) continue;
+            HashSet<Character> set=new HashSet<>();
+            for(int j=u+1;j<v;j++){
+                set.add(s.charAt(j));
+            }
+            ans+=set.size();
+        }
+        return ans;
+        
+
+    }
+}

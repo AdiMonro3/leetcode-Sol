5051e4153de5151aaf4204b78a34be884761e270
AdiMonro3 <165644792+AdiMonro3@users.noreply.github.com>
2026-03-17 13:09:13 +0530

    Create 119-Pascal'sTriangleII

---
+++ 119-Pascal'sTriangleII/version/java/119-Pascal'sTriangleII_20260317_130912.java
@@ -0,0 +1,26 @@
+// Last updated: 17/03/2026, 13:09:12
+1class Solution {
+2    public List<Integer> getRow(int n) {
+3        List<List<Integer>> ans=new ArrayList<>();
+4
+5        for(int i=0;i<n+1;i++){
+6            ans.add(new ArrayList<>());
+7        }
+8
+9        ans.get(0).add(1);
+10
+11        for(int i=1;i<n+1;i++){
+12            int size=ans.get(i-1).size()+1;
+13            for(int j=0;j<size;j++){
+14                if(j==0 || j==size-1) ans.get(i).add(1);
+15                else{
+16                    int prev=ans.get(i-1).get(j-1);
+17                    int curr=ans.get(i-1).get(j);
+18
+19                    ans.get(i).add(prev+curr);
+20                }
+21            }
+22        }
+23        return ans.get(n);
+24    }
+25}
